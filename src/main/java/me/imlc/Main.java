0792e2fbd60b92369beaf71b5f38915786e1f669
Lawrence Ching <lawrence.ching.zh@gmail.com>
2022-11-14 16:51:23 +0800

    initial commit

---
+++ src/main/java/me/imlc/Main.java
@@ -0,0 +1,17 @@
+package me.imlc;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.slf4j.MDC;
+
+import java.util.UUID;
+
+public class Main {
+
+    private static final Logger logger = LoggerFactory.getLogger(Main.class);
+
+    public static void main(String[] args) {
+        MDC.put("logId", UUID.randomUUID().toString());
+        logger.info("Hello world!");
+    }
+}
---
+++ src/test/java/me/imlc/MainTest.java
@@ -0,0 +1,13 @@
+package me.imlc;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTest {
+
+    @Test
+    void test() throws Exception {}{
+        Main.main(new String[]{});
+    }
+}
